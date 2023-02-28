b1e2b620a4723f0a4b6fb0d1d9c08634c8193c5b
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-02-28 10:57:34 -0300

    Cria estrutura do projeto

---
+++ src/constants/colors.js
@@ -0,0 +1,11 @@
+const color = {
+    primaryColor:"#FB6B6B",
+    cardColor: "#FFFFFF",
+    cardFlickedColor:"#FFFFD4",
+    fontColor:"#333333",
+    danger:"#FF3030",
+    warning: "#FF922E",
+    success: "#2FBE34"
+}
+
+export default color
